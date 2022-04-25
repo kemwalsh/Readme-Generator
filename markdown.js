c54ea1a821759fa1980d672d02585aeaa7bdabbd
Kelly Walsh <kellywalsh827@gmail.com>
2022-04-25 00:07:46 -0600

    pseudo code markdown.js

---
+++ markdown.js
@@ -0,0 +1,41 @@
+// Title (readmePrompt.title)
+##
+
+// Licence (readmePrompt.licence.choices??)(badge)
+// get licence badge
+
+// Table of Contents
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Questions](#questions)
+- [Credits](#credits)
+- [Contributing](#contributing)
+- [License](#license)
+
+// Description (readmePrompt.description)
+// Installation (readmePrompt.installation)
+// Usage (readmePrompt.usage)
+// Tests (readmePrompt.tests)
+// Questions (readmePrompt.githubUserName readmePrompt.email)(GitHub/Email)
+// Credits (readmePrompt.credits)
+// Contributing (readmePrompt.contributing)
+// Licence (licenceLink)
+
+
+
+
+// function licenseLink(license) {
+
+//     if (license === 'MIT') {
+//       return "https://lbesson.mit-license.org/"
+//     }
+//     if (license === 'Mozilla') {
+//       return "https://www.mozilla.org/en-US/MPL/2.0/"
+//     }
+//     if (license === 'Apache') {
+//       return "https://www.apache.org/licenses/LICENSE-2.0.txt"
+//     }
+//   }
